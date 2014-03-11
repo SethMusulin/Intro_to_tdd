aa4d7496abf9e64aa43f517cfd2f6617f37da221
your-name <your-email>
2014-03-11 17:13:14 -0600

    Create Array that replaces even numbers with even

--- lib/odd_even.rb
+++ lib/odd_even.rb
@@ -2,11 +2,46 @@ class OddEven
   def initialize (user_number)
     @user_number = user_number
 
-end
+  end
 
   def makearray
     numbers = 1.upto(@user_number).to_a
+    i = 0
 
+    while (i < numbers.length)
+      if (numbers[i] % 2 == 0)
+        numbers[i] = "even"
+      end
 
+      i += 1
+   end
+    numbers
 end
 end
+
+
+
+=begin
+
+class FizzBuzz
+  def initialize(high_limit)
+    @high_limit = high_limit
+  end
+
+  def arrayify
+    numbers = 1.upto(@high_limit).to_a
+
+    i = 0
+
+    while (i < numbers.length)
+      if (numbers[i] % 3 == 0)
+        numbers[i] = "Fizz"
+      end
+
+      i += 1
+    end
+
+    numbers
+  end
+end
+=end
