88d4b38dfb3478d4fc23da1b2230cb33823799da
your-name <your-email>
2014-03-11 23:59:29 -0600

    Sums the numbers in the array after having converted the string to an array

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -12,4 +12,14 @@ describe StringCalc do
   expect(actual).to eq expected
 
   end
+
+it "returns the sum of the numbers in the string" do
+  calc = StringCalc.new("11223")
+
+  expected = 9
+
+  actual = calc.add
+
+  expect(expected).to eq actual
+  end
 end
