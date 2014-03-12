250319d30e8d07418a2ce7e1d61f477c99dd69dc
your-name <your-email>
2014-03-11 21:11:51 -0600

    tests whether program adds 'odds' for odd numbers

--- spec/odd_even_spec.rb
+++ spec/odd_even_spec.rb
@@ -2,35 +2,14 @@ require "odd_even"
 
 describe OddEven do
 
-  it "Creates an array starting at one, ending at initializing number, replaces even numbers in the array with the text 'Even'" do
-  oddeven = OddEven.new(10)
+  it "Creates an array starting at one, ending at initializing number, replaces even numbers in the array with the text 'even' and 'odd' for odd numbers" do
+  oddeven= OddEven.new(10)
 
-  expected = [1,"even", 3, "even", 5, "even", 7, "even", 9, "even"]
+  expected = ["odd","even", "odd", "even", "odd", "even", "odd", "even", "odd", "even"]
 
   actual = oddeven.makearray
 
-    expect(actual).to eq expected
-    end
-end
+  expect(actual).to eq expected
 
-=begin
-require "rspec/core"
-
-require "fizz_buzz"
-
-describe FizzBuzz do
-  it "replaces multiples of 3 with Fizz, multiples of 5 with Buzz, multiples of 15 with FizzBuzz" do
-    fizzbuzz = FizzBuzz.new(15)
-
-    expected = [
-        1, 2, 'Fizz', 4, 'Buzz',
-        'Fizz', 7, 8, 'Fizz', 'Buzz',
-        11, 'Fizz', 13, 14, 'FizzBuzz',
-    ]
-
-    actual = fizzbuzz.arrayify
-
-    expect(actual).to eq expected
   end
 end
-=end
